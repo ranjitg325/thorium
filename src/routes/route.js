bf85e898eacc4d51a15b3013945b87e26e80d70d
ranjitg325 <ranjitg325@gmail.com>
2022-02-25 08:36:42 +0530

    24 feb age solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,51 @@
-//let obj = require('./logger.js')
-let obj1 = require('../loggerfolder/logger.js')
-let obj2= require('../util/helper.js')
-let obj3 = require('../validator/formatter.js')
-
 const express = require('express');
 const router = express.Router();
 
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+  },
+  {
+   name: "SK",
+   age: 20,
+   votingStatus: false
+  },
+  {
+   name: "AA",
+   age: 70,
+   votingStatus: false
+  },
+  {
+   name: "SC",
+   age: 5,
+   votingStatus: false
+  },
+  {
+   name: "HO",
+   age: 40,
+   votingStatus: false
+  }
+  ]
+
+  let eligible=[]
+
+ router.get("/query2",function(req,res){
+   let person = req.query.votingage
+  
+   for (let i=0;i<persons.length;i++){
+     if(persons[i].age>=person){
+        persons[i].votingStatus=true
+        eligible.push(persons[i])
+
+     }
+   }
+   res.send({result:eligible,status:true})
+   console.log(eligible)
+ })  
+
 router.get('/test-me', function (req, res) {
-   // obj.log('thorium')
-   obj1.welcome('welcome')
-   obj2.helper('helper')
-   obj3.trim('trim')
- obj3.convert('convert')
- 
     res.send('Welcome to my api')
 });
 module.exports = router;
