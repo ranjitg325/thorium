7a69a1f04f0b1c3b007bab6ea2e0299cea63f0ff
Nikita-7024 <nikita.singh7024@gmail.com>
2022-03-01 03:48:29 +0530

    assignment 28 feb

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,24 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+    bookName:    {  type: String,
+                    unique: true,
+                    required: true
+    },         
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year:{
+        type:Number,
+        default:2021
+    },
+    tags: [String],
+    authorName: String,
+    totalPages: Number,
+    stockAvailable:Boolean
+
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+
+// const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/bookList", BookController.getBookList)
+
+router.post("/booksInYear",BookController.booksYear)
+
+// router.get("/paticularBooks",BookController.getPaticularBooks)
+
+router.get("/INRBooks", BookController.getInrBooks)
+
+router.get("/randomBooks",BookController.getRandomBooks)
 
 module.exports = router;
