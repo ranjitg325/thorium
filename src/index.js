b536cc4f5ad1d7cfd805a56303f195dee236133d
ranjitg325 <ranjitg325@gmail.com>
2022-03-02 08:39:30 +0530

    1st march assignment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://ranjan:e91pDMx03Sx9wB2V@cluster0.u4idw.mongodb.net/ranjit", {useNewUrlParser: true})
+     .then(() => console.log('mongodb running on 2707'))
+	 .catch(err => console.log(err))
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+	 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
-let persons= [
-   {
-   name: "PK",
-   age: 10,
-   votingStatus: false
-  },
-  {
-   name: "SK",
-   age: 20,
-   votingStatus: false
-  },
-  {
-   name: "AA",
-   age: 70,
-   votingStatus: false
-  },
-  {
-   name: "SC",
-   age: 5,
-   votingStatus: false
-  },
-  {
-   name: "HO",
-   age: 40,
-   votingStatus: false
-  }
-  ]
-
-  let eligible=[]
-
- router.get("/query2",function(req,res){
-   let person = req.query.votingage
-  
-   for (let i=0;i<persons.length;i++){
-     if(persons[i].age>=person){
-        persons[i].votingStatus=true
-        eligible.push(persons[i])
-
-     }
-   }
-   res.send({result:eligible,status:true})
-   console.log(eligible)
- })  
+const allController = require('../controllers/allController')
 
 router.get('/test-me', function (req, res) {
     res.send('Welcome to my api')
 });
+
+
+router.post('/createNewAuthor', allController.createNewAuthor)
+router.post('/createNewBook', allController.createNewBook)
+router.get('/allBooks', allController.allBooks)
+router.get('/authorsName', allController.authorsName)
+router.get('/updatedBookPrice', allController.upadatedBookPrice)
+
+
 module.exports = router;
